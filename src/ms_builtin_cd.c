a5a604a252b353f41855341f2780a98a68a870ac
Gundul <sampah@mailbox.org>
2021-10-26 18:19:31 +0200

    builtin cd done

--- src/ms_builtin_cd.c
+++ src/ms_builtin_cd.c
@@ -6,16 +6,35 @@
 /*   By: graja <graja@student.42wolfsburg.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/26 12:52:00 by graja             #+#    #+#             */
-/*   Updated: 2021/10/26 13:06:46 by graja            ###   ########.fr       */
+/*   Updated: 2021/10/26 18:14:05 by graja            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../header/minishell.h"
 
+static
+char	*ms_checktilde(t_list **head, char *str)
+{
+	if (!ft_strlen(str) || *str != '~')
+		return (str);
+	str++;
+	return (ft_strjoin(ms_getenv(*head, "HOME"), str));
+}
+
 int	ms_builtin_cd(t_list **head, t_split *data)
 {
-	printf("*] cd    ....\n");
-	if (!head || !data)
+	int	err;
+
+	if (!head || !data || !data->tokens[1])
 		return (1);
-	return (0);
+	if (!ft_strlen(data->tokens[1]))
+		err = chdir(ms_getenv(*head, "HOME"));
+	else
+		err = chdir(ms_checktilde(head, data->tokens[1]));
+	if (err)
+		return (err);
+	err = ms_putenv(head, "OLDPWD", ms_getenv(*head, "PWD"));
+	if (!err)
+	       err = ms_putenv(head, "PWD", getcwd(NULL, 0));
+	return (err);
 }
