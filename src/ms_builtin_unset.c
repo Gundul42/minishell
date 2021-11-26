2e7290eeb1773a0e10d3843171f5a6fa6ca000bc
Gundul <sampah@mailbox.org>
2021-11-26 18:27:06 +0100

    fix unset

--- src/ms_builtin_unset.c
+++ src/ms_builtin_unset.c
@@ -6,7 +6,7 @@
 /*   By: graja <graja@student.42wolfsburg.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/27 17:25:06 by graja             #+#    #+#             */
-/*   Updated: 2021/11/26 08:37:30 by graja            ###   ########.fr       */
+/*   Updated: 2021/11/26 18:22:19 by graja            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	ms_namecmp(t_list *lst, char *str)
 	t_env	*ptr;
 
 	ptr = (t_env *)lst->content;
-	if (!ft_strncmp(str, ptr->name, ft_strlen(ptr->name)))
+	if (!ft_strncmp(str, ptr->name, ft_strlen(ptr->name) + 1))
 		return (1);
 	return (0);
 }
@@ -46,6 +46,18 @@ void	ms_unsetvar(t_list **head, char *str)
 	}
 }
 
+static
+int	ms_isvalid(char *s)
+{
+	while (s && *s)
+	{
+		if (!ft_isalnum(*s))
+			return (0);
+		s++;
+	}
+	return (1);
+}
+
 void	ms_delentry(void *lst)
 {
 	t_env	*cnt;
@@ -67,7 +79,15 @@ void	ms_builtin_unset(t_list **head, t_split *data)
 	i = 1;
 	while (ft_strlen(data->tokens[i]))
 	{
-		ms_unsetvar(head, data->tokens[i]);
+		if (ms_isvalid(data->tokens[i]))
+			ms_unsetvar(head, data->tokens[i]);
+		else
+		{
+			printf("unset : \'%s\': not a valid", data->tokens[i]);
+		       	printf(" identifier\n");
+			ms_print_error(head, NULL, 1);
+			break;
+		}
 		i++;
 	}
 	return ;
